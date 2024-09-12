d8e7231e3e970e670de698e1def1fd3980d02020
minafletcher <fletcher.mi@husky.neu.edu>
2024-09-11 23:25:14 -0400

    noise landing v2 test

--- src/components/int-animation/NoiseLanding.js
+++ src/components/int-animation/NoiseLanding.js
@@ -1,53 +1,114 @@
 export default function NoiseLanding(p, props) {
 
   let pg;
+
+  let blobG1;
+  let blobG2;
+  let blobG3;
+  let blobG4;
+
   p.setup = () => {
 
     p.pixelDensity(1)
     p.createCanvas(p.windowWidth, p.windowHeight);
+
+    blobG1 = p.createGraphics(p.windowWidth, p.windowHeight);
+    blobG2 = p.createGraphics(p.windowWidth, p.windowHeight);
+    blobG3 = p.createGraphics(p.windowWidth, p.windowHeight);
+    blobG4 = p.createGraphics(p.windowWidth, p.windowHeight);
+
     pg = p.createGraphics(p.windowWidth, p.windowHeight);
+
 
     // draw all objects relative to center of the screen
     pg.translate(p.windowWidth/2,p.windowHeight/2);
+    blobG1.translate(p.windowWidth/2, p.windowHeight/2);
+    blobG2.translate(p.windowWidth/2, p.windowHeight/2);
+    blobG3.translate(p.windowWidth/2, p.windowHeight/2);
+    blobG4.translate(p.windowWidth/2, p.windowHeight/2);
 
   };
 
   p.draw = () => {
-    pg.blendMode(p.BLEND)
-    pg.background('#FFFDF6');
-    pg.blendMode(p.MULTIPLY)
-    pg.noStroke()
 
-    pg.fill('red');
-    drawBlob(250,-200,12,0.002, 300, 1, 0.02);
-    pg.fill('blue');
-    drawBlob(500,0,14,0.002, 300, -1, 0.05);
-    pg.fill('yellow');
-    drawBlob(700,200,14,0.002, 150, 1, 0.01);
+    pg.clear()
+    blobG1.clear()
+    blobG2.clear()
+    blobG3.clear()
+    blobG4.clear()
+
+    p.blendMode(p.BLEND)
+    p.background('#FFFDF6');
+    p.blendMode(p.MULTIPLY)
+
+    // pg.noStroke()
+
+    // pg.fill('red');
+    // drawBlob(250,-200,12,0.002, 300, 1, 0.02);
+    // pg.fill('blue');
+    // drawBlob(500,0,14,0.002, 300, -1, 0.05);
+    // pg.fill('yellow');
+    // drawBlob(700,200,14,0.002, 150, 1, 0.01);
 
     // drawSquare(250,-150, 15, 15, 10, 10)
-    
 
-    pixelate(pg)
+    // let gradient = p.drawingContext.createLinearGradient(20,20, p.width-20,p.height-20)
+
+    // gradient.addColorStop(0, 'White');
+    // gradient.addColorStop(1, 'Black');
+
+    // p.drawingContext.fillStyle = gradient
+
+    blobG1.noStroke()
+    blobG1.fill('yellow')
+    let xPos1 = 250
+    let yPos1 = -200
+    let size1 = 175
+    drawBlob(blobG1, xPos1,yPos1,12,0.002, size1, 1, 0.03);
+
+    blobG4.noStroke()
+    blobG4.fill('red')
+    drawBlob(blobG4, 450,-75,14,0.002, 275, -1, 0.05);
+
+    blobG2.noStroke()
+    blobG2.fill('blue');
+    drawBlob(blobG2, 500,0,14,0.002, 275, -1, 0.05);
+
+    blobG3.noStroke()
+    blobG3.fill('yellow');
+    drawBlob(blobG3, 700,200,14,0.002, 125, 1, 0.01)
+
+    pixelate(blobG2, 10, 10)
+
+    //fade(blobG1,p.windowWidth/3, p.windowWidth, 0, p.windowHeight, size1, "top-center")
 
     // p.image(pg, 0, 0)
 
     // applyNoiseFilter(pg)
 
-    //grain(pg)
+    //pixelate(pg, 8, 8)
 
-    p.image(pg, 0, 0)
+    p.image(blobG4, 0, 0)
 
+    p.image(blobG1, 0, 0)
+  
+    p.image(blobG3, 0, 0)
+    
+    p.filter(p.BLUR, 10)
+    
+    p.image(blobG2, 0, 0)
+
+    
     };
 
-    function drawBlob(startPosX, startPosY, vertexNum, speed, size, dir, flowDeg){
+    function drawBlob(bg, startPosX, startPosY, vertexNum, speed, size, dir, flowDeg){
 	
-      pg.push();
-      pg.translate(startPosX, startPosY)
+      bg.push();
+      bg.translate(startPosX, startPosY)
       // rotate(frameCount/fcm);
       
       let dr = p.TWO_PI/vertexNum;
-      pg.beginShape();
+      bg.beginShape();
       
       for(let i = 0; i  < vertexNum + 6; i++){
         let ind = i%vertexNum;
@@ -55,10 +116,46 @@ export default function NoiseLanding(p, props) {
         // 0.01 multiplied by sin value controls the amount of degree of flow in the blob
         let r = size + p.noise(p.frameCount*speed + ind) + dir*p.sin(dir*p.frameCount*speed + ind)*(flowDeg*2000);
         // let r = windowWidth*0.1 + noise(frameCount/slow + ind) * windowWidth*0.1 + sin(frameCount/slow + ind)*windowWidth*0.05;
-        pg.curveVertex(p.cos(rad)*r, p.sin(rad)*r);
+        bg.curveVertex(p.cos(rad)*r, p.sin(rad)*r);
       }
-      pg.endShape();
-      pg.pop();
+      bg.endShape();
+      bg.pop();
+    }
+
+    function fade(bg, xPos1, xPos2, yPos1, yPos2, size, fadeMode){
+      bg.loadPixels()
+
+      let offset = 4
+
+      for(let i = 0; i < pg.width; i++){
+        for(let j = 0; j < pg.height; j++){
+          let loc = (i + j * pg.width) * offset;
+
+          if (fadeMode == "top-center"){
+            if (i > xPos1 && i < xPos2 && j > yPos1 && j < yPos2){
+              if (bg.pixels[loc + 3] != 0){
+
+                let dx = p.abs(i - ((xPos2 - xPos1) / 2))
+                let dy = p.abs(j - (yPos1))
+
+                bg.pixels[loc + 3] = 255 - 255 / (dx/dy)
+              }
+              
+            }
+          }
+
+          
+
+          // let r = pg.pixels[loc];       // Red component
+          // let g = pg.pixels[loc + 1];   // Green component
+          // let b = pg.pixels[loc + 2];   // Blue component
+          // let a = pg.pixels[loc + 3];
+
+          // pg.fill(r,g,b,a / i)
+        }
+      }
+
+      bg.updatePixels()
     }
 
     function grain(img){
@@ -80,19 +177,55 @@ export default function NoiseLanding(p, props) {
       img.updatePixels()
     }
 
-    function pixelate(pg){
+    function pixelate(pg, pixelWidth, pixelHeight){
       pg.loadPixels()
 
       let offset = 4
+      let dotA = 20
+      let fadeThreshold = ((pg.width + pg.height * pg.width) * 4) * 0.30
       
-      for(let i = 0; i < pg.width; i+= 8){
-        for(let j = 0; j < pg.height; j+= 8){
+      for(let i = 0; i < pg.width; i+= pixelWidth){
+        for(let j = 0; j < pg.height; j+= pixelHeight){
           let loc = (i + j * pg.width) * offset;
 
-          pg.pixels[loc + 3] = 0
-          pg.pixels[loc + 3 + offset] = 0
-          pg.pixels[loc + 3 + offset*pg.width] = 0
-          pg.pixels[loc + 3 + offset*pg.width + offset] = 0
+          // color pixel dots white if they're inside a colored image
+          if (pg.pixels[loc + 3] != 0){
+            pg.pixels[loc + 3] = 0
+            pg.pixels[loc + 3 + offset] = 0
+            pg.pixels[loc + 3 + offset*pg.width] = 0
+            pg.pixels[loc + 3 + offset*pg.width + offset] = 0
+          }
+           
+          // fade dots based on distance from threshold
+          else if(loc > fadeThreshold) {
+            // alpha is from 0 to 255 (black to white)
+            // set alpha to the inverse distance of the dot from the 90% mark
+            // farther the distance, lighter the alpha
+            // alpha value fades from dotA to 0
+
+            // smaller number / larger number -> larger the number, smaller the result
+            
+            //338700 and 400000 vs 320000
+            // go from 1 to 0
+            // will start bigger, and get smaller as loc grows
+
+            let dy = (fadeThreshold/loc)
+
+            pg.pixels[loc + 3] = dotA * dy
+            pg.pixels[loc + 3 + offset] = dotA * dy
+            pg.pixels[loc + 3 + offset*pg.width] = dotA * dy
+            pg.pixels[loc + 3 + offset*pg.width + offset] = dotA * dy
+          }
+
+          // set pixel dots to dark mode
+          else {
+            pg.pixels[loc + 3] = dotA
+            pg.pixels[loc + 3 + offset] = dotA
+            pg.pixels[loc + 3 + offset*pg.width] = dotA
+            pg.pixels[loc + 3 + offset*pg.width + offset] = dotA
+          }
+
+          
 
           // let r = pg.pixels[loc];       // Red component
           // let g = pg.pixels[loc + 1];   // Green component
