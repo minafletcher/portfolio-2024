4ea61768994a23228435aa78dd4a09db1802dee6
Mina Fletcher <mina@minaparisfletcher.com>
2025-09-18 09:14:42 -0400

    retry mwba image file

--- src/content/content.js
+++ src/content/content.js
@@ -45,7 +45,7 @@ import MailLogo from "./img/svgs/mail-icon.svg";
 import ResumeLogo from "./img/svgs/resume-icon.svg";
 
 // Project Page Images
-import MWBA from "./img/project-images/mwba-thumbnail.png";
+import MWBA from "./img/project-images/mwba-thumbnail.png"; 
 import Enerco from "./img/project-images/enerco-project-2.png";
 import Kaboom from "./img/project-images/campkaboom-min.png";
 import youbet from "./img/project-images/youbet-project-min.png";
